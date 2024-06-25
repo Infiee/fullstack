b7fb40b44fb9089b06cae6422a528c8f94a4a3b4
王飞 <ff349216511@gmail.com>
2024-06-25 18:30:49 +0800

    系统监控

--- apps/admin/src/api/system.ts
+++ apps/admin/src/api/system.ts
@@ -63,25 +63,25 @@ export const getDeptList = async () => {
 /** 获取系统监控-在线用户列表 */
 export const getOnlineLogsList = (data?: object) => {
   // return http.request<ResultTable>("post", "/online-logs", { data });
-  return [];
+  return { data: [] };
 };
 
 /** 获取系统监控-登录日志列表 */
 export const getLoginLogsList = (data?: object) => {
   // return http.request<ResultTable>("post", "/login-logs", { data });
-  return [];
+  return { data: [] };
 };
 
 /** 获取系统监控-操作日志列表 */
 export const getOperationLogsList = (data?: object) => {
   // return http.request<ResultTable>("post", "/operation-logs", { data });
-  return [];
+  return { data: [] };
 };
 
 /** 获取系统监控-系统日志列表 */
 export const getSystemLogsList = (data?: object) => {
   // return http.request<ResultTable>("post", "/system-logs", { data });
-  return [];
+  return { data: [] };
 };
 
 /** 获取系统监控-系统日志-根据 id 查日志详情 */
